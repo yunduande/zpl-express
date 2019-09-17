095d9f9c8d3c0763baf6c69b7b861d73b7972d91
Your Name <you@example.com>
2019-09-17 22:43:54 +0800

    静态资源托管

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const app = express();
 //中间件的调用，这两行代码实现了给 req 身上加一个 body 的属性
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-// app.use(express.json());
-// app.use(express.urlencoded({extented : true}));
-
+//中间件的调用，下面这行代码，实现了给 req 加上一个 cookies 的属性，获取cookie数据
 app.use(cookieParser());
+//中间件使用，静态资源托管管理
+app.use(express.static("public"));
+
 app.get("/",(req,res)=>{
     res.send("hello express")
 });
@@ -23,8 +24,7 @@ app.get("/setCookie",(req,res)=>{
         maxAge:1000 * 60 * 20
     });
     res.send("cookie设置成功");
-
-});
+}); 
 app.get("/getCookie",(req,res)=>{
     console.log(req.cookies);
     res.send("cookie获取成功");
